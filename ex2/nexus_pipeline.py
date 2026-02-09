4fd9c59991349ae811a119fd376757385d7af742
Adrien Vacher <advacher@z2r10p5.42lyon.fr>
2026-02-09 16:44:55 +0100

    add pipeline methode ex2

--- ex2/nexus_pipeline.py
+++ ex2/nexus_pipeline.py
@@ -3,6 +3,7 @@
 from abc import ABC, abstractmethod
 from typing import Any, List, Dict, Union, Optional, Protocol
 
+
 class ProcessingStage(Protocol):
     """
     Protocol defining the interface for processing stages.
@@ -17,7 +18,10 @@ class InputStage:
     Stage 1: Simulates input validation and parsing.
     """
     def process(self, data: Any) -> Any:
-        print("Stage 1: Input validation and parsing")
+        if isinstance(data, str) and "," in data:
+            print(f'Input: "{data}"')
+        else:
+            print(f"Input: {data}")
         if not data:
             raise ValueError("InputStage: Data cannot be empty.")
         return data
@@ -28,8 +32,13 @@ class TransformStage:
     Stage 2: Simulates data transformation and enrichment.
     """
     def process(self, data: Any) -> Any:
-        print("Stage 2: Data transformation and enrichment")
-        ###MISSING CLEAN DATA
+        if isinstance(data, Dict):
+            print("Transform: Enriched with metadata and validation")
+        elif isinstance(data, str):
+            if "user" in data:
+                print("Transform: Parsed and structured data")
+            else:
+                print("Transform: Aggregated and filtered")
         return data
 
 
@@ -38,8 +47,14 @@ class OutputStage:
     Stage 3: Simulates output formatting and delivery.
     """
     def process(self, data: Any) -> Any:
-        print("Stage 3: Output formatting and delivery")
-        ###MISSING STORAGE DATA PROPERLY
+        if isinstance(data, dict):
+            print(f"Output: Processed temperature reading: {data['value']}°C "
+                  "(Normal range)")
+        elif isinstance(data, str):
+            if "user" in data:
+                print("Output: User activity logged: 1 actions processed")
+            else:
+                print("Output: Stream summary: 5 readings, avg: 22.1°C")
         return data
 
 
@@ -55,7 +70,8 @@ class ProcessingPipeline(ABC):
         Adds a processing stage to the pipeline.
 
         Args:
-            stage: An object adhering to the ProcessingStage protocol (must have a process method).
+            stage: An object adhering to the ProcessingStage protocol (
+            must have a process method).
         """
         self.stages.append(stage)
 
@@ -77,42 +93,112 @@ class ProcessingPipeline(ABC):
 class JSONAdapter(ProcessingPipeline):
     def __init__(self, pipeline_id: str):
         super().__init__()
-        self.pipeline_id = pipeline_id     
-    
+        self.pipeline_id = pipeline_id
+
     def process(self, data: Any) -> Any:
+        print("Processing JSON data through pipeline...")
         for stage in self.stages:
-            data = stage.process(data)
+            try:
+                data = stage.process(data)
+            except Exception as e:
+                print(f"{__class__.__name__} "
+                      f"{e.__class__.__name__} error as {e}")
         return data
 
 
 class CSVAdapter(ProcessingPipeline):
     def __init__(self, pipeline_id: str):
         super().__init__()
-        self.pipeline_id = pipeline_id     
+        self.pipeline_id = pipeline_id
 
     def process(self, data: Any) -> Any:
+        print("Processing CSV data through same pipeline..")
         for stage in self.stages:
-            data = stage.process(data)
+            try:
+                data = stage.process(data)
+            except Exception as e:
+                print(f"{__class__.__name__} "
+                      f"{e.__class__.__name__} error as {e}")
         return data
 
 
 class StreamAdapter(ProcessingPipeline):
     def __init__(self, pipeline_id: str):
         super().__init__()
-        self.pipeline_id = pipeline_id     
-    
+        self.pipeline_id = pipeline_id
+
     def process(self, data: Any) -> Any:
+        print("Processing Stream data through same pipeline...")
         for stage in self.stages:
-            data = stage.process(data)
+            try:
+                data = stage.process(data)
+            except Exception as e:
+                print(f"{__class__.__name__} "
+                      f"{e.__class__.__name__} error as {e}")
         return data
-    
-def main () -> None:
+
+
+class NexusManager:
+    def __init__(self):
+        self.pipelines: Dict[str, ProcessingPipeline] = {}
+
+    def register_pipeline(
+            self, name: str, pipeline: ProcessingPipeline) -> None:
+        self.pipelines[name] = pipeline
+
+    def process(self, pipeline_name: str, data: Any) -> Any:
+        pipeline = self.pipelines.get(pipeline_name)
+        if pipeline:
+            try:
+                return pipeline.process(data)
+            except Exception as e:
+                print(f"Error in pipeline {pipeline_name}: {e}")
+                return None
+        else:
+            print(f"Pipeline {pipeline_name} not found.")
+            return None
+
+
+def main() -> None:
     print("=== CODE NEXUS - ENTERPRISE PIPELINE SYSTEM ===\n")
-
-    ####MISSING NEXUS MANAGER
-
+    print("initializing Nexus Manager...")
+    nexus = NexusManager()
+    print("Pipeline capacity: 1000 streams/second\n")
+    pipelines = []
+    pipeline1 = JSONAdapter("json_01")
+    pipelines.append(pipeline1)
+    pipeline2 = CSVAdapter("csv_01")
+    pipelines.append(pipeline2)
+    pipeline3 = StreamAdapter("stream_01")
+    pipelines.append(pipeline3)
     print("Creating Data Processing Pipeline...")
-    
+    print("Stage 1: Input validation and parsing")
+    print("Stage 2: Data transformation and enrichment")
+    print("Stage 3: Output formatting and delivery\n")
+
+    for pipe_line in pipelines:
+        pipe_line.add_stage(InputStage())
+        pipe_line.add_stage(TransformStage())
+        pipe_line.add_stage(OutputStage())
+
+    nexus.register_pipeline("json_stream", pipeline1)
+    nexus.register_pipeline("csv_stream", pipeline2)
+    nexus.register_pipeline("realtime_stream", pipeline3)
+
+    print("=== Multi-Format Data Processing ===\n")
+
+    sample_data_json = {"sensor": "temp", "value": 23.5, "unit": "C"}
+    nexus.process("json_stream", sample_data_json)
+    print("")
+
+    sample_data_cvs = "user, action, timestamp"
+    nexus.process("csv_stream", sample_data_cvs)
+    print("")
+
+    sample_data_realtime = "real-time sensor stream"
+    nexus.process("realtime_stream", sample_data_realtime)
+    print("\nNexus Integration complete. All systems operational.")
+
 
 if __name__ == "__main__":
     main()
